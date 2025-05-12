cafd467e6e17753653387498310bbfb8c8576470
nion2667 <tushinvana@gmail.com>
2025-05-12 23:33:43 +0500

    modified:   main.py 	modified:   pyproject.toml

--- main.py
+++ main.py
@@ -1,16 +1,23 @@
-from subprocess import run
+from subprocess import run, check_output
 from time import localtime
 
 import psutil
 
-# Widget: time, cpu, memory, battery
-WIDGET_ORDER = [
-    "time",
-    "cpu",
-    "memory",
-    "battery",
-]
+# Widget: time, cpu, memory, battery, volume
+WIDGET_ORDER = ["time", "cpu", "memory", "battery", "volume"]
 SEPARATOR = " | "
+
+
+def get_volume():
+    output = check_output(["amixer", "get", "Master"], shell=True).decode()
+    for line in output.split("\n"):
+        if "%" in line:
+            volume_line = line.strip()
+            break
+    status = volume_line.split("[")[2].split("]")[0]
+    volume_percent = volume_line.split("[")[1].split("%")[0]
+    return f"Volume {volume_percent}" if status == "on" else "Mute"
+
 
 def get_time():  # Time
     now = localtime()
@@ -29,7 +36,7 @@ def battery():
     battery = psutil.sensors_battery()
     return (
         f"Charging {battery.percent:0.0f}"
-        if battery.power_plugged
+        if battery.power_plugged and battery.power_plugged != 100
         else f"Bat {battery.percent:0.0f}%"
     )
 
@@ -39,7 +46,9 @@ WIDGET_HANDLERS = {
     "cpu": cpu_load,
     "memory": memory_usage,
     "battery": battery,
+    "volume": get_volume,
 }
+
 
 def metric():
     return SEPARATOR.join(
